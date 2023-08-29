e63d6bbbed8e0b183c378d126e348cce3f75378c
si32 <si32cs50@gmail.com>
2023-08-29 12:41:15 +0300

    9-end

--- py_client/basic.py
+++ py_client/basic.py
@@ -6,3 +6,5 @@ endpoint = 'http://localhost:8000/api/' # localhost:8000/api/
 get_response = requests.get(endpoint, json={'product_id':123})
 print(get_response.status_code)
 print(get_response.json())
+# print(get_response.headers)
+# print(get_response.text)
