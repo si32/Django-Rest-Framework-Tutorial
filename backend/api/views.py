48f3699abb71aedcdee5eb03cd9e72b1d0de5d09
si32 <si32cs50@gmail.com>
2023-08-29 17:17:53 +0300

    12-end

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,12 +6,12 @@ from products.models import Product
 from products.serializers import ProductSerializer
 
 
-@api_view(['GET'])
+@api_view(['POST'])
 def api_home(request, *args, **kwargs):
     """ DRF API View """
-    instance = Product.objects.all().order_by('?').first()
-    data = {}
-    if instance:
-        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
-        data = ProductSerializer(instance).data
-    return Response(data)
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({'invalid':'not a good data'})
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,7 @@ import requests
 # endpoint = 'http://httpbin.org/status/200'
 endpoint = 'http://localhost:8000/api/' # localhost:8000/api/
 
-get_response = requests.get(endpoint, json={'product_id':123})
+get_response = requests.post(endpoint, json={'content':'Hello world'})
 print(get_response.status_code)
 print(get_response.json())
 # print(get_response.headers)
