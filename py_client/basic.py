d7d999471e0418f128828501c3bd61b14e2d21ad
si32 <si32cs50@gmail.com>
2023-08-29 09:35:48 +0300

    5-end

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,7 @@
 import requests
 
-endpoint = 'http://httpbin.org/status/200'
-endpoint = 'http://httpbin.org/anything'
+# endpoint = 'http://httpbin.org/status/200'
+endpoint = 'http://localhost:8000/'
 
 get_response = requests.get(endpoint, json={'query':'hello world'})
 print(get_response.status_code)
